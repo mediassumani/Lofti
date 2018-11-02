37ac7f22cc99630fcbc5495befb12e7ebc8f32d7
Medi Assumani <mediassumani49@gmail.com>
2018-11-01 18:55:51 -0700

    initial commit of the ios client of lofti

---
+++ Lofti/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lofti
+//
+//  Created by Medi Assumani on 11/1/18.
+//  Copyright © 2018 Medi Assumani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
