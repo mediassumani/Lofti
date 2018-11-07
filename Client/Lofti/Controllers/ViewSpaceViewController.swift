583f50750bcd6498c01277b00d67dc14537cc09b
Medi Assumani <mediassumani49@gmail.com>
2018-11-07 13:47:20 -0800

    Add temporary UI elements for the ViewSpaceVC

--- Client/Lofti/Controllers/ViewSpaceViewController.swift
+++ Client/Lofti/Controllers/ViewSpaceViewController.swift
@@ -11,21 +11,24 @@ import UIKit
 
 class ViewSpaceViewController: UIViewController{
     
+    var mainStackView = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        setUpMainStackView()
     }
     
     
     
     //USER INTERFACE
     
+    
     // The textview to show the name of the space
     private let spaceNameTextView: UITextView = {
         let textView = UITextView()
-        let attributedText = NSMutableAttributedString(string: "",attributes: [NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 18)])
+        let attributedText = NSMutableAttributedString(string: "Manzanita Macaroon",attributes: [NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 18)])
         
         textView.attributedText = attributedText
         textView.textAlignment = .center
@@ -38,7 +41,7 @@ class ViewSpaceViewController: UIViewController{
     // The textview to show the renting price
     private let rentingPriceTextView: UITextView = {
         let textView = UITextView()
-        let attributedText = NSMutableAttributedString(string: "",attributes: [NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 15)])
+        let attributedText = NSMutableAttributedString(string: "$7/Hr",attributes: [NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 15)])
         
         textView.attributedText = attributedText
         textView.textAlignment = .center
@@ -51,7 +54,7 @@ class ViewSpaceViewController: UIViewController{
     // The textview to show the description of the place
     private let spaceDescriptionTextView: UITextView = {
         let textView = UITextView()
-        let attributedText = NSMutableAttributedString(string: "",attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 15)])
+        let attributedText = NSMutableAttributedString(string: "The space is a tiny but tidy and well cleaned space with a monitor, 4 chairs, and a white board. Wifi is also available",attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 15)])
         
         textView.attributedText = attributedText
         textView.textAlignment = .center
@@ -64,7 +67,7 @@ class ViewSpaceViewController: UIViewController{
     // The textview to show the time availability of the place
     private let timeAvailabilityTextView: UITextView = {
         let textView = UITextView()
-        let attributedText = NSMutableAttributedString(string: "",attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 15)])
+        let attributedText = NSMutableAttributedString(string: "Wednesday 10th | 12:30 PM - 03:45PM",attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 15)])
         
         textView.attributedText = attributedText
         textView.textAlignment = .center
@@ -77,7 +80,44 @@ class ViewSpaceViewController: UIViewController{
     private let spaceImagesContainerView: UIView = {
        
         let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
         
         return view
     }()
+    
+    private let rentButton: UIButton = {
+        
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Rent", for: .normal)
+        button.layer.cornerRadius = 10
+        button.clipsToBounds = true
+        button.layer.masksToBounds = true
+        button.layer.shadowRadius = 1
+        button.setTitleColor(.gloomyBlue, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    fileprivate func setUpMainStackView(){
+        
+        mainStackView = UIStackView(arrangedSubviews: [spaceNameTextView,
+                                                       rentingPriceTextView,
+                                                       spaceDescriptionTextView,
+                                                       timeAvailabilityTextView,
+                                                       spaceImagesContainerView,
+                                                       rentButton])
+        mainStackView.alignment = .center
+        mainStackView.axis = .vertical
+        mainStackView.distribution = .fillEqually
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mainStackView)
+        
+        NSLayoutConstraint.activate([mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                                     mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+                                     mainStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                                     mainStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)])
+    }
+    
 }
