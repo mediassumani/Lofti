242b66434639a0720e77a9f9404514a677cb88b1
Medi Assumani <mediassumani49@gmail.com>
2018-11-08 13:34:19 -0800

    Added api route to dummy data

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,49 @@
-if(!process.env.PORT){
-  require('dotenv').config();
+
+var express = require("express");
+var http = require("http");
+const app = express()
+
+
+const data = {
+
+    "spaces": [
+      {
+      "name": "Tea Tree Torte",
+      "renter": "Make School",
+      "description": "Cozy small place to work",
+      "address": "555 Post Street, San Francisco, CA",
+      "capacity": 4,
+      "hasWifi": true,
+      "price": 7
+    },
+
+    {
+      "name": "Ozone Studion",
+      "renter": "Trvor Phillips",
+      "description": "Nice Cozy space to study",
+      "address": "246 McAllister Ave, San Francisco, CA",
+      "capacity": 3,
+      "hasWifi": false,
+      "price": 5
+    },
+
+    {
+      "name": "Redwood Backlava",
+      "renter": "Ann Spalding",
+      "description": "Small tidy place to work",
+      "address": "89 George Ave, San Francisco, CA",
+      "capacity": 6,
+      "hasWifi": true,
+      "price": 5
+    }
+  ]
 }
 
-const express = require("express");
-const mongoose = require("mongoose");
-const http = require("http");
-const port = process.env.PORT || 3000;
-const app = express();
+app.get("/api/v1/spaces", function(request, response){
+  response.json(data).send()
+});
 
 
-
-
-app.listen(port);
-module.exports(app);
+app.listen(3000, function(){
+  console.log("Lofti API listening on Port 3000")
+});
