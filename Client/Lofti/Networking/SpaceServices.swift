7920d55022011c227496367f969180addec4b492
Medi Assumani <mediassumani49@gmail.com>
2018-11-20 13:47:06 -0800

    [Add] add space service

---
+++ Client/Lofti/Networking/SpaceServices.swift
@@ -0,0 +1,38 @@
+//
+//  SpaceServices.swift
+//  Lofti
+//
+//  Created by Medi Assumani on 11/20/18.
+//  Copyright © 2018 Medi Assumani. All rights reserved.
+//
+
+import Foundation
+
+struct SpaceServices{
+    
+    /* Implements The CRUD Services of a Reminder model
+     
+     Methods :
+     - index : fetches all spaces nearby
+     */
+    
+    
+    /* Return an array of spaces requested from the Yelp API
+     @param completion ->[Space]: The list of reminder objects to be returned after the method call
+     */
+    static func index(completion: @escaping([Space]?) -> ()){
+        
+        // 1. base link
+        
+        // 2. path
+        
+        // 3. params
+        
+        // 4. header
+        
+        // 5. Http Method
+        
+        // 6. Decode data
+    }
+    
+}
