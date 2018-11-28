783a3280b6fcb0104da7ffa27aad7e2dda69fc9c
Medi Assumani <mediassumani49@gmail.com>
2018-11-27 23:14:22 -0800

    [Add] Collection view with cells

---
+++ Client/Lofti/Views/HomePageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HomePageCollectionViewCell.swift
+//  Lofti
+//
+//  Created by Medi Assumani on 11/27/18.
+//  Copyright © 2018 Medi Assumani. All rights reserved.
+//
+
+import UIKit
+
+class HomePageCollectionViewCell: UICollectionViewCell {
+    
+}
