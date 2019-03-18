fa7f804dea889f39223763a56c0c49f8c6b96962
Medi Assumani <mediassumani49@gmail.com>
2019-03-18 14:15:35 -0700

    [Fix] Codebase architecture

---
+++ Lofti/Networking/HTTP/HTTPNetworkRequest.swift
@@ -0,0 +1,48 @@
+//
+//  HTTPNetworkRequest.swift
+//  ProductHunt
+//
+//  Created by Medi Assumani on 2/16/19.
+//  Copyright © 2019 Medi Assumani. All rights reserved.
+//
+
+import Foundation
+
+public typealias HTTPParameters = [String: Any]
+public typealias HTTPHeaders = [String: String]
+
+struct HTTPNetworkRequest {
+    
+    static let headers =  ["":""]
+    
+    /// Set the body, method, headers, and paramaters of the request
+    static func configureHTTPRequest(from route: HTTPNetworkRoute, with parameters: HTTPParameters, and method: HTTPMethod, contains body: Data?) throws -> URLRequest {
+        
+        guard let url = URL(string: "") else { fatalError("Error while unwrapping url")}
+        
+        var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10.0)
+        
+        request.httpMethod = method.rawValue
+        request.httpBody = body
+        try configureParametersAndHeaders(parameters: parameters, headers: headers, request: &request)
+        
+        return request
+    }
+    
+    /// Configure the request parameters and headers before the API Call
+    static func configureParametersAndHeaders(parameters: HTTPParameters?,
+                                         headers: HTTPHeaders?,
+                                         request: inout URLRequest) throws {
+
+        do {
+
+            if let headers = headers, let parameters = parameters {
+                try URLEncoder.encodeParameters(for: &request, with: parameters)
+                try URLEncoder.setHeaders(for: &request, with: headers)
+            }
+        } catch {
+            throw HTTPNetworkError.encodingFailed
+        }
+    }
+    
+}
