7266fda389f537d060ac9d0a9cff9976c2799085
Medi Assumani <mediassumani49@gmail.com>
2019-03-18 14:20:09 -0700

    [Fix] remove sensative data from github

--- Lofti/Helpers/Constant.swift
+++ Lofti/Helpers/Constant.swift
@@ -10,18 +10,22 @@ import Foundation
 
 // This File contains constants used throughout the application
 
-struct Constant{
+enum Constant{
     
-    
-    static let homePageCellID: String = "homepagecellidentifier"
-    static let userPreferencesCellID: String = "userPreferencesCellID"
-    static let onboardingViewCellIdentifier: String = "onboardingCellId"
+    // Constants for Networking operations
     static let YELP_API_KEY: String = "gY2YUHqCE92WGXAVa_c9pCZfaBSXksbKJKIPOxwf_99Nrt1TVonoN2Fyk7YZffBFADtn8da2LbWQdA02G6O8QGXWb8psGP4nVaFXPz4q6QwuwLUwaZ6tA58uZ3n0W3Yx"
     static let YELP_CLIENT_ID: String = "_GWT2CXiyyxccGtUChLa6Q"
     static let SEARCH_LIMIT = 10
     static let PAUSIBLE_PREFERENCES = ["Libraries", "Coffee Shops", "Book Stores", "Shared Offices", "Parks", "Comunity Centers"]
     static let WEATHER_API_KEY: String = "d708f84ed5ebce9e7b93b60a35faf289"
     static let WEATHER_URL: URL = URL(string: "https://api.darksky.net/forecast/")!
+    
+    
+    // Constants for UI related operations
+    static let homePageCellID: String = "homepagecellidentifier"
+    static let userPreferencesCellID: String = "userPreferencesCellID"
+    static let onboardingViewCellIdentifier: String = "onboardingCellId"
+    
     
     static let pages = [
         Page(imageName: "Logo_onboarding", header: "Welcome to Lofti", description: "Lofti helps you find your ideal study and working spaces nearby based on your preferences."),
