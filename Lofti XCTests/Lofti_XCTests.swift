ffaa139ca3445e375bbbfcecb7f8789bb5aac57a
Medi Assumani <mediassumani49@gmail.com>
2019-03-20 10:31:05 -0700

    [Add] empty methods to be tested

--- Lofti XCTests/Lofti_XCTests.swift
+++ Lofti XCTests/Lofti_XCTests.swift
@@ -7,27 +7,61 @@
 //
 
 import XCTest
+@testable import Pods_Lofti
 
 class Lofti_XCTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // Model Testing
+    func testSpaceModel(){
+        
+    }
+    
+    func testAddressModel(){
+        
+    }
+    
+    func testWeatherModel(){
+        
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    
+    // Network Testing
+    func testLocationServices(){
+        
     }
-
+    
+    func testSpaceServices(){
+        
+    }
+    
+    func testWeatherServices(){
+        
+    }
+    
+    func testURLEncoder(){
+        
+    }
+    
+    func testHTTPNeworkRequest(){
+        
+    }
+    
+    func testHTTPNetwotkResponse(){
+        
+    }
+    
+    func testHTTPNetworkError(){
+        
+    }
 }
