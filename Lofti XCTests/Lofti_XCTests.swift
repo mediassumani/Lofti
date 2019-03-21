39b3f33209dbc906b52c7d06b616821e57670e7e
Medi Assumani <mediassumani49@gmail.com>
2019-03-20 21:01:33 -0700

    d

---
+++ Lofti XCTests/Lofti_XCTests.swift
@@ -0,0 +1,67 @@
+//
+//  Lofti_XCTests.swift
+//  Lofti XCTests
+//
+//  Created by Medi Assumani on 3/20/19.
+//  Copyright © 2019 Medi Assumani. All rights reserved.
+//
+
+import XCTest
+@testable import Pods_Lofti
+
+class Lofti_XCTests: XCTestCase {
+
+    override func setUp() {
+        
+        
+    }
+
+    override func tearDown() {
+        
+        
+    }
+
+    // Model Testing
+    func testSpaceModel(){
+        
+    }
+    
+    func testAddressModel(){
+        
+    }
+    
+    func testWeatherModel(){
+        
+    }
+
+    
+    
+    // Network Testing
+    func testLocationServices(){
+        
+    }
+    
+    func testSpaceServices(){
+        
+    }
+    
+    func testWeatherServices(){
+        
+    }
+    
+    func testURLEncoder(){
+        
+    }
+    
+    func testHTTPNeworkRequest(){
+        
+    }
+    
+    func testHTTPNetwotkResponse(){
+        
+    }
+    
+    func testHTTPNetworkError(){
+        
+    }
+}
